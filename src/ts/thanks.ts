e668982d22b0ec248a6935d18f51eb1e761e6d84
Belov Roman <rnbelov96@gmail.com>
2021-10-01 16:43:02 +0300

    Edit thanks.html (V2)

--- src/ts/thanks.ts
+++ src/ts/thanks.ts
@@ -1,5 +1,4 @@
 import '../scss/thanks.scss';
-// import './modals';
 
 const nameLabelEl = document.querySelector('.js-name') as HTMLSpanElement;
 nameLabelEl.textContent = localStorage.getItem('userName') || 'Гость';
